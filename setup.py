3317ff11be1f6883008e0e33aeb33da088465978
James Buchanan <james42buchanan@gmail.com>
2024-02-06 10:01:44 +0000

    Fixed naming bug in setup.py.

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name = 'dpypr',
     version = '1.0.0',
-    description = '(data-piper) a data pipeline convenience module for Python.',
+    description = '(data-piper) a data pipeline convenience module for Python',
     author = 'James Buchanan',
     author_email = 'buchananja.github@pm.me',
     license = 'MIT',
-    url = 'https://github.com/buchananja/pyped',
+    url = 'https://github.com/buchananja/dpypr',
     install_requires = [
         'pandas',
         'pyarrow'
