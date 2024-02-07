f44624defdf978be7eaec8b159f719e0f86debb8
buchananja <buchananja.github@pm.me>
2024-02-07 14:04:46 +0000

    Improved formatting for readability and added better logging.

--- dpypr/read.py
+++ dpypr/read.py
@@ -3,121 +3,269 @@ The 'read' module contains functionality for reading data of various datatypes
 into data pipelines.
 '''
 
+
 # Dependencies ################################################################
 import pandas as pd
 import os
 import sqlite3
 import dpypr as dp
 
+
 # Data Loading ################################################################
-def read_all_json(path):
-    r'''
-    Iteratively loads data.json from the data directory and assign to 
-    dataframes. Unpacks dictionary to global variables names with the format 
-    df_{filename}.
+# def read_everything(path):
+#     '''
+#     #################### all doc strings are wrong.
+#     '''
+#     files = os.listdir(path)
+#     data_dictionary = dict()
+#     for file in files:
+#             if file.endswith('.json'):
+#                 df = pd.read_json(os.path.join(path, file))
+#                 filename = os.path.splitext(file)[0]
+#                 data_dictionary[f'df_{filename}'] = df
+#             elif file.endswith('.csv'):
+#                 df = pd.read_csv(os.path.join(path, file))
+#                 filename = os.path.splitext(file)[0]
+#                 data_dictionary[f'df_{filename}'] = df
+#             elif file.endswith('.xlsx'):
+#                 df = pd.read_excel(os.path.join(path, file))
+#                 filename = os.path.splitext(file)[0]
+#                 data_dictionary[f'df_{filename}'] = df
+#             elif file.endswith('.feather'):
+#                 df = pd.read_feather(os.path.join(path, file))
+#                 filename = os.path.splitext(file)[0]
+#                 data_dictionary[f'df_{filename}'] = df
+#             elif file.endswith('.parquet'):
+#                 df = pd.read_parquet(os.path.join(path, file))
+#                 filename = os.path.splitext(file)[0]
+#                 data_dictionary[f'df_{filename}'] = df
+#             elif file.endswith('.pickle'):
+#                 df = pd.read_pickle(os.path.join(path, file))
+#                 filename = os.path.splitext(file)[0]
+#                 data_dictionary[f'df_{filename}'] = df
+#     if not data_dictionary:
+#         dp.sleep_log('No files read.')
+#     return data_dictionary
+        
+        
+def read_all_json(path, messaging = True, sleep_seconds = 0.1):
     '''
+    - Iteratively loads all json files from the data directory and assigns to 
+    dataframes. 
+    - Unpacks dictionary to global variables names with the format 
+    df_filename.
+    - Messaging logs statements about number of records.
+    '''
+    
     files = os.listdir(path)
     data_dictionary = dict()
+    
+    if messaging:
+        dp.sleep_log('\nWriting data...', sleep_time = sleep_seconds)
+    
     for file in files:
         if file.endswith('.json'):
             df = pd.read_json(os.path.join(path, file))
             filename = os.path.splitext(file)[0]
             data_dictionary[f'df_{filename}'] = df
+            
+            if messaging:
+                dp.sleep_log(
+                        f'- read {f'df_{filename}'} ({len(filename):,}) records.',
+                        sleep_time = sleep_seconds 
+                    )
+                
     if not data_dictionary:
         dp.sleep_log('No files read.')
+    if messaging:
+        dp.sleep_log('All data read successfully.\n', sleep_time = sleep_seconds)
+        
     return data_dictionary
 
-def read_all_csv(path):
-    r'''
-    Iteratively loads data.csv from the data directory and assign to 
-    dataframes. Unpacks dictionary to global variables names with the format 
-    df_{filename}.
+
+def read_all_csv(path, messaging = True, sleep_seconds = 0.1):
     '''
+    - Iteratively loads all csv files from the data directory and assigns to 
+    dataframes. 
+    - Unpacks dictionary to global variables names with the format 
+    df_filename.
+    - Messaging logs statements about number of records.
+    '''
+    
     files = os.listdir(path)
     data_dictionary = dict()
+    
+    if messaging:
+        dp.sleep_log('\nWriting data...', sleep_time = sleep_seconds)
+        
     for file in files:
         if file.endswith('.csv'):
             df = pd.read_csv(os.path.join(path, file))
             filename = os.path.splitext(file)[0]
             data_dictionary[f'df_{filename}'] = df
+            
+            if messaging:
+                dp.sleep_log(
+                        f'- read {f'df_{filename}'} ({len(filename):,}) records.',
+                        sleep_time = sleep_seconds 
+                    )
+                
     if not data_dictionary:
         dp.sleep_log('No files read.')
+    if messaging:
+        dp.sleep_log('All data read successfully.\n', sleep_time = sleep_seconds)
+        
     return data_dictionary
 
-def read_all_xlsx(path):
-    r'''
-    Iteratively loads data.xlsx from the data directory and assign to 
-    dataframes. Unpacks dictionary to global variables names with the format 
-    df_{filename}.
+
+def read_all_xlsx(path, messaging = True, sleep_seconds = 0.1):
     '''
+    - Iteratively loads all xlsx files from the data directory and assigns to 
+    dataframes. 
+    - Unpacks dictionary to global variables names with the format 
+    df_filename.
+    - Messaging logs statements about number of records.
+    '''
+    
     files = os.listdir(path)
     data_dictionary = dict()
+    
+    if messaging:
+        dp.sleep_log('\nWriting data...', sleep_time = sleep_seconds)
+        
     for file in files:
         if file.endswith('.xlsx'):
             df = pd.read_excel(os.path.join(path, file))
             filename = os.path.splitext(file)[0]
             data_dictionary[f'df_{filename}'] = df
+            
+            if messaging:
+                dp.sleep_log(
+                        f'- read {f'df_{filename}'} ({len(filename):,}) records.',
+                        sleep_time = sleep_seconds 
+                    )
+                
     if not data_dictionary:
         dp.sleep_log('No files read.')
+    if messaging:
+        dp.sleep_log('All data read successfully.\n', sleep_time = sleep_seconds)
+        
     return data_dictionary
 
-def read_all_feather(path):
-    r'''
-    Iteratively loads data.feather from the data directory and assign to 
-    dataframes. Unpacks dictionary to global variables names with the format 
-    df_{filename}.
+
+def read_all_feather(path, messaging = True, sleep_seconds = 0.1):
     '''
+    - Iteratively loads all feather files from the data directory and assigns to 
+    dataframes. 
+    - Unpacks dictionary to global variables names with the format 
+    df_filename.
+    - Messaging logs statements about number of records.
+    '''
+    
     files = os.listdir(path)
     data_dictionary = dict()
+    
+    if messaging:
+        dp.sleep_log('\nWriting data...', sleep_time = sleep_seconds)
+        
     for file in files:
         if file.endswith('.feather'):
             df = pd.read_feather(os.path.join(path, file))
             filename = os.path.splitext(file)[0]
             data_dictionary[f'df_{filename}'] = df
+            
+            if messaging:
+                dp.sleep_log(
+                        f'- read {f'df_{filename}'} ({len(filename):,}) records.',
+                        sleep_time = sleep_seconds 
+                    )
+                
     if not data_dictionary:
         dp.sleep_log('No files read.')
+    if messaging:
+        dp.sleep_log('All data read successfully.\n', sleep_time = sleep_seconds)
+        
     return data_dictionary
 
-def read_all_parquet(path):
-    r'''
-    Iteratively loads data.parquet from the data directory and assign to 
-    dataframes. Unpacks dictionary to global variables names with the format 
-    df_{filename}.
+
+def read_all_parquet(path, messaging = True, sleep_seconds = 0.1):
     '''
+    - Iteratively loads all parquet files from the data directory and assigns to 
+    dataframes. 
+    - Unpacks dictionary to global variables names with the format 
+    df_filename.
+    - Messaging logs statements about number of records.
+    '''
+    
     files = os.listdir(path)
     data_dictionary = dict()
+    
+    if messaging:
+        dp.sleep_log('\nWriting data...', sleep_time = sleep_seconds)
+        
     for file in files:
         if file.endswith('.parquet'):
             df = pd.read_parquet(os.path.join(path, file))
             filename = os.path.splitext(file)[0]
             data_dictionary[f'df_{filename}'] = df
+            
+            if messaging:
+                dp.sleep_log(
+                        f'- read {f'df_{filename}'} ({len(filename):,}) records.',
+                        sleep_time = sleep_seconds 
+                    )
+                
     if not data_dictionary:
         dp.sleep_log('No files read.')
+    if messaging:
+        dp.sleep_log('All data read successfully.\n', sleep_time = sleep_seconds)
+        
     return data_dictionary
 
-def read_all_pickle(path):
-    r'''
-    Iteratively loads data.pickle from the data directory and assign to 
-    dataframes. Unpacks dictionary to global variables names with the format 
-    df_{filename}.
+
+def read_all_pickle(path, messaging = True, sleep_seconds = 0.1):
     '''
+    - Iteratively loads all pickle files from the data directory and assigns to 
+    dataframes. 
+    - Unpacks dictionary to global variables names with the format 
+    df_filename.
+    - Messaging logs statements about number of records.
+    '''
+    
     files = os.listdir(path)
     data_dictionary = dict()
+    
+    if messaging:
+        dp.sleep_log('\nWriting data...', sleep_time = sleep_seconds)
+        
     for file in files:
         if file.endswith('.pickle'):
             df = pd.read_pickle(os.path.join(path, file))
             filename = os.path.splitext(file)[0]
             data_dictionary[f'df_{filename}'] = df
+            
+            if messaging:
+                dp.sleep_log(
+                        f'- read {f'df_{filename}'} ({len(filename):,}) records.',
+                        sleep_time = sleep_seconds 
+                    )
+                
     if not data_dictionary:
         dp.sleep_log('No files read.')
+    if messaging:
+        dp.sleep_log('All data read successfully.\n', sleep_time = sleep_seconds)
+        
     return data_dictionary
+     
               
 def read_all_sqlite(path):
-    r'''
+    '''
     Returns all data from a sqlite database as a dictionary.
     '''
+    
     conn = sqlite3.connect(path)
     cur = conn.cursor()
+    
     # queries all tables in database
     cur.execute('''
         SELECT name 
@@ -132,22 +280,29 @@ def read_all_sqlite(path):
         query = f"SELECT * FROM {table_name[0]}"
         data_dictionary[table_name[0]] = pd.read_sql_query(query, conn)
     conn.close()
+    
     if not data_dictionary:
         dp.sleep_log('No files read.')
+        
     return data_dictionary     
 
+
 def gather_data_dictionary(globals_dict):
-    r'''
+    '''
     Packages all pandas dataframes in input dictionary beginning with 'df\_' and returns
     output dictionary.
     '''
+    
     data_dictionary = dict()
+    
     for name, data in globals_dict.items():
         if name.startswith('df_') and isinstance(data, pd.DataFrame):
             data_dictionary.update({name: data})
     if not data_dictionary:
         dp.sleep_log('No files found.')
+        
     return data_dictionary
+
 
 def unpack_data_dictionary(
         data_dictionary,
@@ -155,11 +310,12 @@ def unpack_data_dictionary(
         sleep_seconds = 0, 
         messaging = False,
     ):
-    r'''
+    '''
     - Loads all dataframes from data_dictionary into output_dictionary if 
     argument present, returns output_dictionary if not.
-    - Logs number of records to console for each file if messaging is True.
+    - Messaging logs statements about number of records.
     '''
+    
     if not output_dictionary:
         output_dictionary = dict()
         
@@ -171,4 +327,5 @@ def unpack_data_dictionary(
                             f'- Read df_{key} ({len(value):,}) records.', 
                             sleep_time = sleep_seconds
                         )
+                        
     return output_dictionary
--- dpypr/write.py
+++ dpypr/write.py
@@ -3,168 +3,188 @@ The 'write' module contains functionality for writing data of various datatypes
 from data pipelines.
 '''
 
+
 # Dependencies ################################################################
 import dpypr as dp
 import sqlite3
 from sqlite3 import OperationalError
 import os
 
+
 # Data Writing ################################################################
 def write_dict_to_json(
         globals_dict, 
         path, 
-        file_prefix = '',
+        file_prefix = 'out',
         messaging = True,
         sleep_seconds = 0.1
     ):
     r'''
     - Writes all objects beginning with 'df\_' in global space to path as .json. 
     - Prefix allows user to rename processed files upon writing.
-    - 'Messaging' allows user to output number of records to console.
+    - Messaging logs statements about number of records.
     '''
+    
     if messaging:
-        dp.sleep_log('\nWriting data...', sleep_time = 1)
+        dp.sleep_log('\nWriting data...', sleep_time = sleep_seconds)
+        
     for name, data in globals_dict.items():
         if name.startswith('df_'):
             data.to_json(f'{path}/{file_prefix}_{name[3:]}.json')
-            # ouputs filenames and number or records to console
+
             if messaging:
                 dp.sleep_log(
                     f'- Wrote {file_prefix}_{name[3:]} ({len(data):,}) records.', 
                     sleep_time = sleep_seconds
                     )
     if messaging:
-        dp.sleep_log('All data written successfully.\n', sleep_time = 1)
+        dp.sleep_log('All data written successfully.\n', sleep_time = sleep_seconds)
+
 
 def write_dict_to_csv(
         globals_dict, 
         path, 
-        file_prefix = '',
+        file_prefix = 'out',
         messaging = True,
         sleep_seconds = 0.1
     ):
     r'''
     - Writes all objects beginning with 'df\_' in global space to path as .csv. 
     - Prefix allows user to rename processed files upon writing.
-    - 'Messaging' allows user to output number of records to console.
+    - Messaging logs statements about number of records.
     '''
+    
     if messaging:
-        dp.sleep_log('\nWriting data...', sleep_time = 1)
+        dp.sleep_log('\nWriting data...', sleep_time = sleep_seconds)
+        
     for name, data in globals_dict.items():
         if name.startswith('df_'):
             data.to_csv(f'{path}/{file_prefix}_{name[3:]}.csv')
-            # ouputs filenames and number or records to console
+
             if messaging:
                 dp.sleep_log(
                     f'- Wrote {file_prefix}_{name[3:]} ({len(data):,}) records.', 
                     sleep_time = sleep_seconds
                     )
     if messaging:
-        dp.sleep_log('All data written successfully.\n', sleep_time = 1)
+        dp.sleep_log('All data written successfully.\n', sleep_time = sleep_seconds)
+       
             
 def write_dict_to_xlsx(
         globals_dict, 
         path, 
-        file_prefix = '',
+        file_prefix = 'out',
         messaging = True,
         sleep_seconds = 0.1
     ):
     r'''
     - Writes all objects beginning with 'df\_' in global space to path as .xlsx. 
     - Prefix allows user to rename processed files upon writing.
-    - 'Messaging' allows user to output number of records to console.
+    - Messaging logs statements about number of records.
     '''
+    
     if messaging:
-        dp.sleep_log('\nWriting data...', sleep_time = 1)
+        dp.sleep_log('\nWriting data...', sleep_time = sleep_seconds)
+        
     for name, data in globals_dict.items():
         if name.startswith('df_'):
             data.to_excel(f'{path}/{file_prefix}_{name[3:]}.xlsx')
-            # ouputs filenames and number or records to console
+
             if messaging:
                 dp.sleep_log(
                     f'- Wrote {file_prefix}_{name[3:]} ({len(data):,}) records.', 
                     sleep_time = sleep_seconds
                     )
     if messaging:
-        dp.sleep_log('All data written successfully.\n', sleep_time = 1)
+        dp.sleep_log('All data written successfully.\n', sleep_time = sleep_seconds)
+
 
 def write_dict_to_feather(
         globals_dict, 
         path, 
-        file_prefix = '',
+        file_prefix = 'out',
         messaging = True,
         sleep_seconds = 0.1
     ):
     r'''
     - Writes all objects beginning with 'df\_' in global space to path as .feather. 
     - Prefix allows user to rename processed files upon writing.
-    - 'Messaging' allows user to output number of records to console.
+    - Messaging logs statements about number of records.
     '''
+    
     if messaging:
-        dp.sleep_log('\nWriting data...', sleep_time = 1)
+        dp.sleep_log('\nWriting data...', sleep_time = sleep_seconds)
+        
     for name, data in globals_dict.items():
         if name.startswith('df_'):
             data.to_feather(f'{path}/{file_prefix}_{name[3:]}.feather')
-            # ouputs filenames and number or records to console
+
             if messaging:
                 dp.sleep_log(
                     f'- Wrote {file_prefix}_{name[3:]} ({len(data):,}) records.', 
                     sleep_time = sleep_seconds
                     )
     if messaging:
-        dp.sleep_log('All data written successfully.\n', sleep_time = 1)
+        dp.sleep_log('All data written successfully.\n', sleep_time = sleep_seconds)
+        
             
 def write_dict_to_parquet(
         globals_dict, 
         path, 
-        file_prefix = '',
+        file_prefix = 'out',
         messaging = True,
         sleep_seconds = 0.1
     ):
     r'''
     - Writes all objects beginning with 'df\_' in global space to path as .parquet. 
     - Prefix allows user to rename processed files upon writing.
-    - 'Messaging' allows user to output number of records to console.
+    - Messaging logs statements about number of records.
     '''
+    
     if messaging:
-        dp.sleep_log('\nWriting data...', sleep_time = 1)
+        dp.sleep_log('\nWriting data...', sleep_time = sleep_seconds)
+        
     for name, data in globals_dict.items():
         if name.startswith('df_'):
             data.to_parquet(f'{path}/{file_prefix}_{name[3:]}.parquet')
-            # ouputs filenames and number or records to console
+
             if messaging:
                 dp.sleep_log(
                     f'- Wrote {file_prefix}_{name[3:]} ({len(data):,}) records.', 
                     sleep_time = sleep_seconds
                     )
     if messaging:
-        dp.sleep_log('All data written successfully.\n', sleep_time = 1)
+        dp.sleep_log('All data written successfully.\n', sleep_time = sleep_seconds)
+        
         
 def write_dict_to_pickle(
         globals_dict, 
         path, 
-        file_prefix = '',
+        file_prefix = 'out',
         messaging = True,
         sleep_seconds = 0.1
     ):
     r'''
     - Writes all objects beginning with 'df\_' in global space to path as .pickle. 
     - Prefix allows user to rename processed files upon writing.
-    - 'Messaging' allows user to output number of records to console.
+    - Messaging logs statements about number of records.
     '''
+    
     if messaging:
-        dp.sleep_log('\nWriting data...', sleep_time = 1)
+        dp.sleep_log('\nWriting data...', sleep_time = sleep_seconds)
+        
     for name, data in globals_dict.items():
         if name.startswith('df_'):
             data.to_pickle(f'{path}/{file_prefix}_{name[3:]}.pickle')
-            # ouputs filenames and number or records to console
+
             if messaging:
                 dp.sleep_log(
                     f'- Wrote {file_prefix}_{name[3:]} ({len(data):,}) records.', 
                     sleep_time = sleep_seconds
                     )
     if messaging:
-        dp.sleep_log('All data written successfully.\n', sleep_time = 1)
+        dp.sleep_log('All data written successfully.\n', sleep_time = sleep_seconds)
+          
                 
 def write_dict_to_sqlite(
         data_dictionary, 
@@ -178,8 +198,9 @@ def write_dict_to_sqlite(
     - Writes all objects beginning with 'df\_' in global space to path as
     tables in sqlite database. 
     - Prefix allows user to rename processed files upon writing.
-    - 'Messaging' allows user to output number of records to console.
+    - Messaging logs statements about number of records.
     '''
+    
     if overwrite:
         # remove old database
         try:
@@ -188,13 +209,15 @@ def write_dict_to_sqlite(
             dp.sleep_log('WARNING: File is being used by another process!')
         except FileNotFoundError:
             dp.sleep_log('WARNING: File does not exist!')
-            
-    # connect to database 
+     
+    # connect to database       
     conn = sqlite3.connect(path)
     cur = conn.cursor()
+    
     try:
         if messaging:
-            dp.sleep_log('\nWriting data...', sleep_time = 1)
+            dp.sleep_log('\nWriting data...', sleep_time = sleep_seconds)
+            
         # create tables in new database
         for name, data in data_dictionary.items():
             if name.startswith('df_'):
@@ -202,6 +225,7 @@ def write_dict_to_sqlite(
                 cur.execute(f'''
                     CREATE TABLE {file_prefix}_{name[3:]} ({cols})
                 ''')
+                
                 if messaging:
                     dp.sleep_log(
                             f'- Wrote {file_prefix}_{name[3:]} ({len(data):,}) records.', 
@@ -209,7 +233,9 @@ def write_dict_to_sqlite(
                         )
         # write tables to new database
         conn.commit()
+        
         if messaging:
-            dp.sleep_log('All data written successfully.\n', sleep_time = 1)
+            dp.sleep_log('All data written successfully.\n', sleep_time = sleep_seconds)
+            
     except OperationalError:
         dp.sleep_log('WARNING: Database already exists!')
