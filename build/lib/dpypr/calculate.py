e5f29bee4d13b82094d2c7dc27e1b1bd930e12d0
buchananja <buchananja.github@pm.me>
2024-03-01 09:57:48 +0000

    Updated calculate

---
+++ build/lib/dpypr/calculate.py
@@ -0,0 +1,16 @@
+'''
+the 'calculate' module contains functionality for doing statistical and
+mathematical operations on data
+'''
+
+def rate_of_change(df_row, col1, col2, default_rate):
+    '''
+    calculates the rate of change between two columns by row with default rate
+    able to be specified; typically used with an apply function
+    '''
+    
+    if df_row[col1] > 0:
+        result = (df_row[col1] - df_row[col2]) / df_row[col1]
+    else:
+        result = default_rate
+    return result
